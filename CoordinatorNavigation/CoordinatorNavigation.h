4216b9e1421035c2e496d7d1b630323e5c26b960
Jaafar <als-92@hotmail.com>
2022-11-05 19:19:51 +0100

    Initial Commit

---
+++ CoordinatorNavigation/CoordinatorNavigation.h
@@ -0,0 +1,18 @@
+//
+//  CoordinatorNavigation.h
+//  CoordinatorNavigation
+//
+//  Created by Jaafar on 05/11/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CoordinatorNavigation.
+FOUNDATION_EXPORT double CoordinatorNavigationVersionNumber;
+
+//! Project version string for CoordinatorNavigation.
+FOUNDATION_EXPORT const unsigned char CoordinatorNavigationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CoordinatorNavigation/PublicHeader.h>
+
+
